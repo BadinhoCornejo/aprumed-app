9d64f6345578b1a647ecb2779ad03a5e9cab247c
Badinho <Cornejo>
2021-10-21 16:18:21 -0500

    add validator for phone number

--- src/app/components/int-newuser/int-newuser.component.ts
+++ src/app/components/int-newuser/int-newuser.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { MatSnackBar } from "@angular/material";
 import { UsersService } from "src/app/services/users/users.service";
 import { MustMatch } from "../../_helpers/must-match.validator";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-int-newuser",
@@ -27,10 +28,12 @@ export class IntNewuserComponent implements OnInit {
   };
   tiposUsuario: any = [];
   sexos: any = ["Masculino", "Femenino"];
+
   constructor(
     private formBuilder: FormBuilder,
     private userService: UsersService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private router: Router
   ) {
     this.getTiposUsuario();
     this.form = this.formBuilder.group(
@@ -40,7 +43,10 @@ export class IntNewuserComponent implements OnInit {
 
         sexo: [this.usuario.sexo, Validators.required],
 
-        telefono: [this.usuario.telefono, Validators.required],
+        telefono: [
+          this.usuario.telefono,
+          [Validators.required, Validators.maxLength(9)],
+        ],
         tipoUsuarioID: ["", Validators.required],
         email: [this.usuario.email, [Validators.required, Validators.email]],
         usrPassword: [
@@ -87,15 +93,23 @@ export class IntNewuserComponent implements OnInit {
     this.usuario.telefono = this.form.value.telefono;
     this.usuario.tipoUsuario.tipoUsuarioID = this.form.value.tipoUsuarioID;
 
+    const action = "VOLVER";
+
     this.userService.addUser(this.usuario).subscribe(
       (result) => {
-        let action = "VOLVER";
-        this.snackBar.open("Usuario registrado!", action, {
-          duration: 2000,
-        });
+        this.snackBar
+          .open("Usuario registrado!", action, {
+            duration: 2000,
+          })
+          .onAction()
+          .subscribe(() => {
+            this.router.navigateByUrl("admin/users");
+          });
       },
       (error) => {
-        console.error(JSON.stringify(error));
+        this.snackBar.open("Error!", action, {
+          duration: 2000,
+        });
       }
     );
   }
